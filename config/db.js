05e499477c517c95b4e0cdba85d071abd891b73e
nvineng <naveen211063cse@sdbc.ac.in>
2024-12-21 22:43:44 +0530

    Add a new secret to the .env file.

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const mySqlPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Naveen@18",
-  database: "student_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = mySqlPool.promise();
